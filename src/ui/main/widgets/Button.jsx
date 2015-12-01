41a7167727cc601a995a28326c4f5cbb17f8a1f4
Christopher Janietz <janietz@media-saturn.com>
2015-12-01 07:41:14 +0100

    Added button

---
+++ src/ui/main/widgets/Button.jsx
@@ -0,0 +1,17 @@
+import {mixin} from '../../../util/mixin'
+import {TLoggable} from '../../../util/logging/TLoggable'
+
+let React = window.React
+let $ = window.$
+
+export class Button extends mixin(React.Component, TLoggable){
+    constructor() {
+        super()
+    }
+
+    render() {
+        return(
+            <button type="button" className="btn btn-default waves-effect">{this.props.text}</button>
+        )
+    }
+}
